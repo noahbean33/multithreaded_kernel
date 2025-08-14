fd7e2ef164a14b36729186bc409a4c85373b0757
Noah Bean <92399044+noahbean33@users.noreply.github.com>
2025-08-13 19:32:24 -0700

    implemented disk driver

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include "idt/idt.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -80,6 +81,9 @@ void kernel_main()
 
     // Initialize the heap
     kheap_init();
+
+    // Search and initialize the disks
+    disk_search_and_init();
 
     // Initialize the interrupt descriptor table
     idt_init();
